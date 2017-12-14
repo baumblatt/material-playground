78f85841d6367c99cd0a38566da05de4287b44c4
Bernardo Baumblatt <baumblatt@gmail.com>
2017-12-14 02:00:11 -0200

    Cordova firsts plugins, Ionic Native and Device Ready detection.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+
+import {Device} from '@ionic-native/device';
+
+import 'rxjs/add/operator/do';
+import {BehaviorSubject} from 'rxjs/BehaviorSubject';
+import {Observable} from 'rxjs/Observable';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+	title = 'app';
+	platform$: BehaviorSubject<string>;
+
+	constructor(private device: Device) {
+		this.platform$ = new BehaviorSubject<string>('not yet');
+
+		Observable.fromEvent(document, 'deviceready')
+				.do(() => console.log('deviceready'))
+				.subscribe(() => this.platform$.next(this.device.platform));
+	}
+
+	log() {
+		console.log(this.platform$.getValue());
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
+import {Device} from '@ionic-native/device';
 
-import { AppComponent } from './app.component';
-
+import {AppComponent} from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+	],
+	providers: [Device],
+	bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
