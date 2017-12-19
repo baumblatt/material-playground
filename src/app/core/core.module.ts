18d756ca6cdf47b62038427b20185f750b727464
Bernardo Baumblatt <baumblatt@gmail.com>
2017-12-19 01:34:10 -0200

    Router to home on core.

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,14 @@
+import {CommonModule} from '@angular/common';
+import {NgModule} from '@angular/core';
+
+import {CoreRoutingModule} from '@app/core/core-routing.module';
+
+@NgModule({
+	imports: [
+		CommonModule,
+		CoreRoutingModule
+	],
+	declarations: []
+})
+export class CoreModule {
+}
