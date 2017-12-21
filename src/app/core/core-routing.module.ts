2b59317914b49458adbe615d8af8f3c681c7ca1f
Bernardo Baumblatt <bernardob@br-petrobras.com.br>
2017-12-21 09:33:13 -0200

    Reorganize Core module and route module.

--- src/app/core/core-routing.module.ts
+++ src/app/core/core-routing.module.ts
@@ -2,16 +2,14 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
 import {HomeComponent} from '@app/core/pages/home/home.component';
-import {MaterialModule} from '@app/material/material.module';
 
 const routes: Routes = [
 	{path: '', component: HomeComponent}
 ];
 
 @NgModule({
-	imports: [MaterialModule, RouterModule.forChild(routes)],
-	exports: [RouterModule],
-	declarations: [HomeComponent]
+	imports: [RouterModule.forChild(routes)],
+	exports: [RouterModule]
 })
 export class CoreRoutingModule {
 }
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,13 +2,16 @@ import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
 
 import {CoreRoutingModule} from '@app/core/core-routing.module';
+import {HomeComponent} from '@app/core/pages/home/home.component';
+import {MaterialModule} from '@app/material/material.module';
 
 @NgModule({
 	imports: [
 		CommonModule,
-		CoreRoutingModule
+		CoreRoutingModule,
+		MaterialModule
 	],
-	declarations: []
+	declarations: [HomeComponent]
 })
 export class CoreModule {
 }
