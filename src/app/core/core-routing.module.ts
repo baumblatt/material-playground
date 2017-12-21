af1e9fedc6c5f08ec6d087adf8c2fd0ab6efe91d
Bernardo Baumblatt <bernardob@br-petrobras.com.br>
2017-12-21 10:57:52 -0200

    Layout component.

--- src/app/core/core-routing.module.ts
+++ src/app/core/core-routing.module.ts
@@ -1,10 +1,17 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {LayoutComponent} from '@app/core/components/layout/layout.component';
 
 import {HomeComponent} from '@app/core/pages/home/home.component';
 
 const routes: Routes = [
-	{path: '', component: HomeComponent}
+	{path: '', pathMatch: 'full', redirectTo: 'home'},
+	{
+		path: '', component: LayoutComponent,
+		children: [
+			{path: 'home', component: HomeComponent}
+		]
+	},
 ];
 
 @NgModule({
