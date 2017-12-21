b0f0ac20d29042950ebb889a8a7e2db662364a21
Bernardo Baumblatt <baumblatt@gmail.com>
2017-12-21 00:24:24 -0200

    The material module to import material components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import {CordovaService} from '@app/cordova.service';
+import {MaterialModule} from '@app/material/material.module';
 
 import {SplashScreen} from '@ionic-native/splash-screen';
 
@@ -16,7 +17,8 @@ import {AppComponent} from './app.component';
 	imports: [
 		BrowserModule,
 		BrowserAnimationsModule,
-		AppRoutingModule
+		AppRoutingModule,
+		MaterialModule
 	],
 	providers: [CordovaService, SplashScreen],
 	bootstrap: [AppComponent]
---
+++ src/app/material/material.module.ts
@@ -0,0 +1,9 @@
+import {NgModule} from '@angular/core';
+import {MatToolbarModule} from '@angular/material';
+
+@NgModule({
+	imports: [MatToolbarModule],
+	declarations: []
+})
+export class MaterialModule {
+}
