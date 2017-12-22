79e9a5698cd0db32ca8d68a8bc29dfa463a19da2
Bernardo Baumblatt <bernardob@br-petrobras.com.br>
2017-12-22 16:46:07 -0200

    Material module under shared folder.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,7 +3,7 @@ import {NgModule} from '@angular/core';
 import {LayoutComponent} from '@app/core/components/layout/layout.component';
 import {CoreRoutingModule} from '@app/core/core-routing.module';
 import {HomeComponent} from '@app/core/pages/home/home.component';
-import {SharedModule} from '@app/shared/shared.module';
+import {SharedModule} from '@shared/shared.module';
 
 @NgModule({
 	imports: [
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,7 +1,7 @@
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
 
-import {MaterialModule} from '@app/material/material.module';
+import {MaterialModule} from '@shared/material/material.module';
 
 @NgModule({
 	imports: [CommonModule, MaterialModule],
