332292cabb34a252813a6ae7847d6cc8ed91a0d6
Bernardo Baumblatt <baumblatt@gmail.com>
2017-12-25 11:57:09 -0200

    Sidenav layout implementation.

--- src/app/core/core-routing.module.ts
+++ src/app/core/core-routing.module.ts
@@ -1,13 +1,21 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+
 import {LayoutComponent} from '@app/core/components/layout/layout.component';
+import {SidenavLayoutComponent} from '@app/core/components/sidenav-layout/sidenav-layout.component';
 
 import {HomeComponent} from '@app/core/pages/home/home.component';
 
 const routes: Routes = [
 	{path: '', pathMatch: 'full', redirectTo: 'home'},
 	{
-		path: '', component: LayoutComponent,
+		path: '', component: SidenavLayoutComponent,
+		children: [
+			{path: 'home', component: HomeComponent}
+		]
+	},
+	{
+		path: 'plain', component: LayoutComponent,
 		children: [
 			{path: 'home', component: HomeComponent}
 		]
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,13 +4,14 @@ import {LayoutComponent} from '@app/core/components/layout/layout.component';
 import {CoreRoutingModule} from '@app/core/core-routing.module';
 import {HomeComponent} from '@app/core/pages/home/home.component';
 import {SharedModule} from '@shared/shared.module';
+import { SidenavLayoutComponent } from './components/sidenav-layout/sidenav-layout.component';
 
 @NgModule({
 	imports: [
 		SharedModule,
 		CoreRoutingModule
 	],
-	declarations: [HomeComponent, LayoutComponent]
+	declarations: [HomeComponent, LayoutComponent, SidenavLayoutComponent]
 })
 export class CoreModule {
 }
