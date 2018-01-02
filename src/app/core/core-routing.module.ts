5e5b1f072cfd1fd2fce3061c23309d9bc8492d06
Bernardo Baumblatt <baumblatt@gmail.com>
2018-01-02 02:45:38 -0200

    Double Navigation implementation.

--- src/app/core/core-routing.module.ts
+++ src/app/core/core-routing.module.ts
@@ -2,16 +2,25 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
 import {BottomNavLayoutComponent} from '@app/core/components/bottom-nav-layout/bottom-nav-layout.component';
+import {DoubleNavLayoutComponent} from '@app/core/components/double-nav-layout/double-nav-layout.component';
 import {LayoutComponent} from '@app/core/components/layout/layout.component';
 import {SidenavLayoutComponent} from '@app/core/components/sidenav-layout/sidenav-layout.component';
 
 import {HomeComponent} from '@app/core/pages/home/home.component';
 
 const routes: Routes = [
-	{path: '', pathMatch: 'full', redirectTo: 'home'},
 	{
-		path: '', component: SidenavLayoutComponent,
+		path: '', component: DoubleNavLayoutComponent,
 		children: [
+			{path: '', pathMatch: 'full', redirectTo: 'home'},
+			{path: 'home', component: HomeComponent},
+			{path: 'sample', loadChildren: 'app/sample/sample.module#SampleModule'}
+		]
+	},
+	{
+		path: 'side', component: SidenavLayoutComponent,
+		children: [
+			{path: '', pathMatch: 'full', redirectTo: 'home'},
 			{path: 'home', component: HomeComponent},
 			{path: 'sample', loadChildren: 'app/sample/sample.module#SampleModule'}
 		]
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,18 +1,19 @@
 import {NgModule} from '@angular/core';
 
 import {BottomNavLayoutComponent} from '@app/core/components/bottom-nav-layout/bottom-nav-layout.component';
+import {DoubleNavLayoutComponent} from '@app/core/components/double-nav-layout/double-nav-layout.component';
 import {LayoutComponent} from '@app/core/components/layout/layout.component';
+import {SidenavLayoutComponent} from '@app/core/components/sidenav-layout/sidenav-layout.component';
 import {CoreRoutingModule} from '@app/core/core-routing.module';
 import {HomeComponent} from '@app/core/pages/home/home.component';
 import {SharedModule} from '@shared/shared.module';
-import {SidenavLayoutComponent} from './components/sidenav-layout/sidenav-layout.component';
 
 @NgModule({
 	imports: [
 		SharedModule,
 		CoreRoutingModule
 	],
-	declarations: [HomeComponent, BottomNavLayoutComponent, LayoutComponent, SidenavLayoutComponent]
+	declarations: [HomeComponent, BottomNavLayoutComponent, DoubleNavLayoutComponent, LayoutComponent, SidenavLayoutComponent]
 })
 export class CoreModule {
 }
